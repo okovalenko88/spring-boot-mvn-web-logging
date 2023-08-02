c2404543b8a3f462c4191900db7669fef6ffd4c5
Oleksandr Kovalenko <okovalenko1988@gmail.com>
2023-08-02 18:38:43 +0200

    start

--- src/main/java/com/sap/cc/greeting/GreetingController.java
+++ src/main/java/com/sap/cc/greeting/GreetingController.java
@@ -1,28 +1,40 @@
 package com.sap.cc.greeting;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
+@Slf4j
 public class GreetingController {
 
 	private GreetingService service;
+	// private Logger logger = LoggerFactory.getLogger(getClass());
 
 	public GreetingController(GreetingService service) {
 		this.service = service;
 	}
 
 	@GetMapping("/hello")
-	public String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return getGreeting("Hello", name);
+	public String greeting(@RequestParam(value = "name", defaultValue = "Worldd") String name) {
+		try {
+			MDC.put("path", "/hello");
+			return getGreeting("Hello", name);
+		} finally {
+			MDC.clear();
+		}
 	}
 
 	@GetMapping("/howdy")
 	public String deprecatedGreeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-		System.err.println("Deprecated endpoint used.");
+		log.error("Deprecated endpoint used.");
 		return getGreeting("Howdy", name);
 	}
 
